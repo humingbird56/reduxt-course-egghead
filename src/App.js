0c256a71e74ee432b8cd989885637f0e3765246a
andrew <sbstn.andrew56@gmail.com>
2018-08-12 23:12:01 +0700

    create a simple redux test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React with redux</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          <TodoForm/>
+          <TodoList todos={this.props.todos}/>
+        </div>
       </div>
     );
   }
